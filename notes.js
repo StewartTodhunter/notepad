54443216d5c3e1850a70001dbe5a128bb5fdfc8a
charisse-gebhart <charisse_gebhart@hotmail.com>
2013-07-14 14:44:10 +1000

    removed refs to overlay div: not needed

--- notes.js
+++ notes.js
@@ -1,24 +1,18 @@
 function newNote() {
 
-	var overlay = document.createElement("div");
 	var dialogue = document.createElement("div");
-
-	overlay.setAttribute("id","overlay");
-	//overlay.setAttribute("class", "overlay");
 
 	dialogue.setAttribute("id","dialogue");
 	//dialogue.setAttribute("class", "dialogue");
 
 	dialogue.innerHTML = "<form id=\"inputForm\" onSubmit=\"return false\">Title:<input id=\"noteTitle\" type=\"text\"></input><br/><textarea id=\"inputArea\" rows=\"20\"></textarea><br/><input type=\"submit\" value=\"submit\" onclick=\"applyNote(noteTitle.value, inputArea.value)\"><input type=\"submit\" value=\"cancel\" onclick=\"endOverlay()\"></form>";
 
-	document.body.appendChild(overlay);
 	document.body.appendChild(dialogue);	
-
 }
 
 function applyNote(name, text) {
 
-	endOverlay();
+	endDialogue();
 
 	var noteArea = document.getElementById("notes");
 	var listItem = document.createElement("div");
@@ -28,14 +22,14 @@ function applyNote(name, text) {
 	noteArea.insertBefore(listItem, noteArea.firstChild);
 }
 
-function endOverlay() {
+function endDialogue() {
 	document.body.removeChild(document.getElementById("overlay"));
 	document.body.removeChild(document.getElementById("dialogue"));
 }
 
 function applyNote(title, text){
 
-	endOverlay();
+	endDialogue();
 	
 	var note = document.createElement("div");
 	var header = document.createElement("H3");
