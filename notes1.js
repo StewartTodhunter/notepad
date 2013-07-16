c3399f0d2014c798248083747ad672fdc0569cb9
Narmada <bnarmada.vrse@gmail.com>
2013-07-17 09:31:47 +1000

    Working Changes
    Reverting the conflicts created due to multiple checkins last night

--- notes1.js
+++ notes1.js
@@ -1,31 +1,58 @@
 function getFocus() {
-	document.getElementById("noteTitle").focus()
+	document.getElementById("msgTitle").focus()
 }
 
-function addNote() {
-	var note = document.createElement("Button");
-	var txt = document.createTextNode("new note");
-	var element = document.getElementById("notes");
-	note.setAttribute("id","new");
-	note.setAttribute("onclick", "newNote(this.id)");
-	note.setAttribute("type", "submit");
-	note.appendChild(txt);
-	//document.body.appendChild(note);
-	element.insertBefore(note,element.firstChild);
-}
-function newNote(id) {
+function newNote(identity) {
 	var dialogue = document.createElement("div");
 	dialogue.setAttribute("id","dialogue");
-	if (id=="new") {
-		dialogue.innerHTML = "<form id=\"inputForm\" onSubmit=\"return false\">Title:<input id=\"noteTitle\" type=\"text\"></input><br/><textarea id=\"inputArea\" ></textarea><br/><input class=\"dialogueButtons\" type=\"submit\" value=\"submit\" onclick=\"applyNote(noteTitle.value, inputArea.value)\"><input class=\"dialogueButtons\" type=\"submit\" value=\"cancel\" onclick=\"endDialogue()\"></form>";
-	}
-	else
-	{
+	
+	var br1 = document.createElement("br");
+	var br2 = document.createElement("br");
+	var titleHeading = document.createElement("span");
+	titleHeading.innerHTML = "Title: ";
+	
+	var titleField = document.createElement("input");
+	titleField.setAttribute("type", "text");
+	titleField.setAttribute("id","msgTitle");
+	
+	var textArea = document.createElement("textarea");
+	textArea.setAttribute("id","msg");
+	textArea.rows = "10";
+	textArea.cols = "80";
+	
+	dialogue.appendChild(titleHeading);
+	dialogue.appendChild(titleField);
+	dialogue.appendChild(br1);
+	dialogue.appendChild(textArea);
+	//dialogue.appendChild(br2);
+	
+	var sButton = document.createElement("input");
+	sButton.setAttribute("type", "Submit");
+	sButton.setAttribute("value", "Submit");
+	
+	var sRemove = document.createElement("input");
+	sRemove.setAttribute("type", "Submit");
+	sRemove.setAttribute("value", "Remove");
+	sRemove.setAttribute("onclick","noteRemove(msgTitle.value)");
+		
+	var sCancel = document.createElement("input");
+	sCancel.setAttribute("type", "submit");
+	sCancel.setAttribute("value", "Cancel");
+	sCancel.setAttribute("onclick","endDialogue()");
+	if (identity!="clickToCreateNewNote") {
 		var noteCont = new Array();
-		noteCont = ReadDb(id);
-		//alert(noteCont[0]);
-		dialogue.innerHTML = "<form id=\"inputForm\" onSubmit=\"return false\">Title:<input id=\"noteTitle\" value=noteCont[0] type=\"text\"></input><br/><textarea id=\"inputArea\" value=noteCont[1]></textarea><br/><input class=\"dialogueButtons\" type=\"submit\" value=\"submit\" onclick=\"applyNote(noteTitle.value, inputArea.value)\"><input class=\"dialogueButtons\" type=\"submit\" value=\"cancel\" onclick=\"endDialogue()\"></form>";
+		noteCont = ReadDb(identity);
+		//dialogue.innerHTML = "<form id=\"inputForm\" onSubmit=\"return false\">Title:<input id=\"noteTitle\" value='noteCont.title' type=\"text\"></input><br/><textarea id=\"inputArea\" rows=\"20\" value=noteCont.body></textarea><br/><input type=\"submit\" value=\"submit\" onclick=\"applyNote(noteTitle.value, inputArea.value)\"><input type=\"submit\" value=\"cancel\" onclick=\"endDialogue()\"></form>";
+		titleField.setAttribute("value",noteCont.title);
+		textArea.value = noteCont.body;
+		sButton.setAttribute("onclick","modifyNote(msgTitle.value, msg.value)");
 	}
+	else{
+		sButton.setAttribute("onclick","applyNote(msgTitle.value, msg.value)");
+	}
+	dialogue.appendChild(sButton);
+	dialogue.appendChild(sRemove);
+	dialogue.appendChild(sCancel);
 	document.body.appendChild(dialogue);
 	getFocus();
 }
@@ -33,66 +60,63 @@ function endDialogue() {
 	document.body.removeChild(document.getElementById("dialogue"));
 }
 
+function noteRemove(title) {
+	localStorage.removeItem(title);
+	var element = document.getElementById(title);
+	element.parentNode.removeChild(element);
+	endDialogue();
+}
+
 function applyNote(title, text){
 	endDialogue();
-	var newModDate = new Date().toString('dd-mm-yyyy h:mm:ss');
-	var updateTitle = document.getElementById("new");
-	updateTitle.innerHTML = title + " " + newModDate;
-	updateTitle.setAttribute("id",title);
-	writeDb(title, text, newModDate);
+	var ModDate = new Date().toString('dd-mm-yyyy h:mm:ss');
+	var note = document.createElement("Button");
+	var txt = document.createTextNode(title + " " + ModDate);
+	var element = document.getElementById("notes");
+	note.setAttribute("id",title);
+	note.setAttribute("onclick", "newNote(this.id)");
+	note.setAttribute("type", "submit");
+	note.appendChild(txt);
+	//document.body.appendChild(note);
+	element.insertBefore(note,element.firstChild);
+	writeDb(title, text, ModDate);
 }
-//creating the database
-var db = openDatabase('notesDb', '1.0', 'Test DB', 2 * 1024 * 1024);
+
+function modifyNote(title, text){
+	endDialogue();
+	var ModDate = new Date().toString('dd-mm-yyyy h:mm:ss');
+	writeDb(title, text, ModDate);
+}
 
 function writeDb(Title,Body, NTime)
 {
-	db.transaction(function (tx) 
-	{
-		tx.executeSql('CREATE TABLE IF NOT EXISTS ourNotes (id unique, Title, Body, CreateDate, ModDate)');
-	});
-	
-	// Can't insert a row that already exists so find the number of rows and increase id by 1.
-	var newId;
-	db.transaction(function (tx) 
-	{
-		tx.executeSql('SELECT * FROM ourNotes', [], function (tx, results) 
-			{
-			var len = results.rows.length, i;
-			newId = len + 1;
-			}, null);
-	});
-
-
-	db.transaction(function (tx) 
-	{
-		var tempTitle = Title;
-		var tempBody = Body;
-		var tempCreateDate = new Date().toString('dd-mm-yyyy h:mm:ss');
-		var tempModDate = NTime;
-		tx.executeSql('INSERT INTO ourNotes (id, Title, Body, CreateDate, ModDate) VALUES (?, ?, ?, ?, ?)', [newId, tempTitle, tempBody, tempCreateDate, tempModDate]);
-		msg = '<p>Log message created and row inserted.</p>';
-		document.querySelector('#status').innerHTML =  msg;
-	});
+	var notes = {};
+	notes.title = Title;
+	notes.body = Body;
+	if (!JSON.parse( localStorage.getItem(Title) )) {
+		notes.createdDate = new Date().toString('dd-mm-yyyy h:mm:ss');
+	}
+	notes.modifiedDate = NTime;
+	localStorage.setItem( Title, JSON.stringify(notes) );
 }
 
 function ReadDb(id)
 {
-	var check = id;
-	var notesContent = new Array();
-	db.transaction(function (tx) 
-	{
-		tx.executeSql('SELECT * FROM ourNotes',[], function (tx, results) 
-		{
-			var len = results.rows.length, i;
-			for (i = 0; i < len; i++)
-			{
-				notesContent[0]=results.rows.item(i).Title;
-				notesContent[1]=results.rows.item(i).Body;
-				notesContent[2]=results.rows.item(i).CreateDate;
-				notesContent[3]=results.rows.item(i).ModDate;
-				//alert(notesContent[0]);
-			}
-		}, null);
-	});
-	return notesContent;
+	var noteStore = {};
+	noteStore = JSON.parse( localStorage.getItem( id ) );
+	return noteStore;
 }
+
+function notesLoad() {
+	var localStorageKeys = Object.keys(localStorage);
+	if(localStorageKeys)
+	{
+		for (var i=1; i < localStorageKeys.length; i++)  {
+			alert(localStorage.getItem(localStorageKeys[i]));
+		}
+	}
+	else
+	{
+		alert("Please create a note, you don't have one stored now.");
+	}
+}	
